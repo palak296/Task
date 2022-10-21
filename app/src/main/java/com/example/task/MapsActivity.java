b4565509ce2eab265e6965e9886d9e1a93fda792
Palak Garg <palakgarg296@gmail.com>
2022-10-21 16:33:49 +0530

    file upload

---
+++ app/src/main/java/com/example/task/MapsActivity.java
@@ -0,0 +1,79 @@
+package com.example.task;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.task.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Delhi and move the camera
+        LatLng delhi = new LatLng(28.704510420823382, 76.7746177472934);
+        mMap.addMarker(new MarkerOptions().position(delhi).title("Marker in Delhi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
+        mMap.addGroundOverlay(new GroundOverlayOptions()
+                .position(delhi, 1000f, 1000f).image(BitmapDescriptorFactory.fromResource(R.drawable.img))
+                .clickable(true)
+        );
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(delhi).title("clicked here"));
+                Geocoder geocoder = new Geocoder();
+                try {
+                    ArrayList<Address> arrAdr = (ArrayList<Address>) geocoder.getFromLocation();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+        }
+
+    }
